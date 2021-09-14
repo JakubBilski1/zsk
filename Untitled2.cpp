b53e458c8770d4e98600e8b944161db9b878ca80
JakubBilski1 <90185194+JakubBilski1@users.noreply.github.com>
2021-09-14 15:45:15 +0200

    Add files via upload

--- Untitled2.cpp
+++ Untitled2.cpp
@@ -1,37 +1,50 @@
 #include <iostream>
+#include <locale.h>
 using namespace std;
-/* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
-class Worker {
+class Worker{
 	public:
-// deklaracja zmiennych cz³onkowskich (w³aœciwoœci)
-	string name="Janusz";
+//deklaracja zmiennych cz3onkowskich
+	string name;
 	string surname;
-// zezwala na znak, tylko dodatnia liczba do  32767
-	unsigned short age;
+	string nationality;
+	string alldata;
+	unsigned short int yearBirthday;
+	char gender;
+	float height;
+	float a;
 	
-// deklaracja funkcji cz³onkowskiej (metoda)
-	void showData(){
-		cout << "Imiê" << endl;
-	};
-	
-	void showName(){
-		cout << name << endl;
-	}
+// definicja funkcji cz3onkowskiej
+
+
+	void showSurname();
+	void showPersonality();
+	void showAllData();
+	void showName();
+
+};
+
+// deklaracja metody (prototyp)
+
+
+
+//definicja metody show surname
+
+float Worker::showSurname(){
+				float wynik;
+				cin>>a;
+				wynik=a*a;
+				return wynik;
 };
 
 int main(int argc, char** argv) {
-	setlocale(LC_CTYPE, "Polish");
-	
+	setlocale(LC_CTYPE, "polish");
 	Worker pracownik;
-	
-	cout << pracownik.name << endl;
-	pracownik.showData();
-	pracownik.surname = "Kowalski";
-	pracownik.name = "Andrzej";
-	cout << "imiê i nazwisko:" << pracownik.name <<" "<< pracownik.surname << endl;
-	cout << "wiek" << " " << pracownik.age;
+	pracownik.surname ="Nowak";
+	pracownik.name ="Marek";
+	pracownik.showSurname();
 	pracownik.showName();
+	pracownik.showPersonality();
 	return 0;
 }
 
